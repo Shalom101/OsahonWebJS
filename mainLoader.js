33ec43e4d331d2cea6576093dfbf61a1accf1628
Shalom101 <shalomizebhor@gmail.com>
2023-11-06 19:35:03 +0100

    Update mainLoader.js
    udpated duration time

--- mainLoader.js
+++ mainLoader.js
@@ -4,7 +4,7 @@ let customEase =
 let counter = {
   value: 0
 };
-let loaderDuration = 8;
+let loaderDuration = 5;
 
 // If not a first time visit in this tab
 if (sessionStorage.getItem("visited") !== null) {
